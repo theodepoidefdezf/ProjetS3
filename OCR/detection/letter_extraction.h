8624b5dae9d4acc686906696cf1e1af7122ec5af
Greegg75 <94235083+Greegg75@users.noreply.github.com>
2025-10-31 13:47:10 +0100

    Clean up comments in letter_extraction.h
    Removed unnecessary comment block from letter_extraction.h

--- OCR/detection/letter_extraction.h
+++ OCR/detection/letter_extraction.h
@@ -3,12 +3,6 @@
 
 #include "../utils/structs.h"
 
-/**
- * @brief Extrait chaque lettre d'une image contenant une liste de mots.
- *
- * @param word_list_image L'image (struct Image) contenant SEULEMENT la liste de mots.
- * @return Une structure 'Letters' contenant les images de chaque lettre.
- */
 Letters extract_letters(Image word_list_image);
 
-#endif // LETTER_EXTRACTION_H
+#endif
