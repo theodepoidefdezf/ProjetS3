f8589fe2550530a3e8f5fde729c95f47a3e8e2db
Eric <eric.ren@epita.fr>
2025-12-02 17:38:02 +0100

    Solver100%

--- OCR/Solver/main.c
+++ OCR/Solver/main.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
@@ -8,44 +8,68 @@ int main(int argc, char *argv[])
 {
     if (argc != 3)
     {
-        fprintf(stderr, "Usage: %s <fichier_grille> <mot>\n", argv[0]);
+        fprintf(stderr, "Usage: %s <fichier_grille> <fichier_mots>\n", argv[0]);
         return 1;
     }
 
-    const char *nom_fichier = argv[1];
-    char mot_raw[MAX_MOT];
-    int i;
-
-    for (i = 0; i < MAX_MOT - 1 && argv[2][i]; i++)
-    {
-        mot_raw[i] = toupper((unsigned char) argv[2][i]);
-    }
-    mot_raw[i] = '\0';
+    const char *fichier_grille = argv[1];
+    const char *fichier_mots = argv[2];
 
     static char grille[MAX_LIGNES][MAX_COLONNES];
     int lignes = 0;
     int colonnes = 0;
 
-    if (!lire_grille(nom_fichier, grille, &lignes, &colonnes))
+    if (!lire_grille(fichier_grille, grille, &lignes, &colonnes))
     {
-        fprintf(stderr, "Erreur: impossible de lire la grille depuis '%s'\n", nom_fichier);
+        fprintf(stderr, "Erreur: impossible de lire la grille depuis '%s'\n",
+                fichier_grille);
         return 1;
     }
 
-    MotTrouve resultat;
-    chercher_dans_grille(lignes, colonnes, grille, mot_raw, &resultat);
-
-    if (resultat.trouve)
+    FILE *fm = fopen(fichier_mots, "r");
+    if (!fm)
     {
-        printf("(%d,%d)(%d,%d)\n",
-               resultat.start_colonne, resultat.start_ligne,
-               resultat.end_colonne, resultat.end_ligne);
-    }
-    else
-    {
-        printf("Not Found\n");
+        fprintf(stderr, "Erreur: impossible d'ouvrir le fichier mots '%s'\n",
+                fichier_mots);
+        return 1;
     }
 
+    char buffer[MAX_MOT];
+
+    while (fgets(buffer, sizeof(buffer), fm))
+    {
+        // Nettoyage fin de ligne
+        int len = strlen(buffer);
+        while (len > 0 && (buffer[len - 1] == '\n' || buffer[len - 1] == '\r'))
+            buffer[--len] = '\0';
+
+        // Mot vide → ignorer
+        if (len == 0)
+            continue;
+
+        // Transformer en majuscules
+        char mot[MAX_MOT];
+        int i;
+        for (i = 0; i < MAX_MOT - 1 && buffer[i]; i++)
+            mot[i] = toupper((unsigned char)buffer[i]);
+        mot[i] = '\0';
+
+        MotTrouve resultat;
+        chercher_dans_grille(lignes, colonnes, grille, mot, &resultat);
+
+        if (resultat.trouve)
+        {
+            printf("%s: (%d,%d)(%d,%d)\n",
+                   mot,
+                   resultat.start_colonne, resultat.start_ligne,
+                   resultat.end_colonne, resultat.end_ligne);
+        }
+        else
+        {
+            printf("%s: Not Found\n", mot);
+        }
+    }
+
+    fclose(fm);
     return 0;
 }
-
